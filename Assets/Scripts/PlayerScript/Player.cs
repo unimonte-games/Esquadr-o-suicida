22e22c1bd104e0269273ae61ebb871881526c105
Jordan Savio <jordan.savio.95@gmail.com>
2020-03-16 20:54:03 -0300

    1.0.1f - Scripts Ajustados
    Scripts de Movimento e portas, bugaram na transiçao. Foram arrumados.

---
+++ Assets/Scripts/PlayerScript/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool PlayerType; //true = Player1 // false = Player2
+   
+    public int Heart = 3; //Vida do jogador em coracoes
+    public float LifeBar = 100f; //Vida do jogador em Numero
+    public float ManaBar = 100f; //Energia do jogador para usar as armas
+    public float DefeseBar = 100f; //Defesa do jogador
+
+     
+    void Start()
+    {
+        
+    }
+
+   
+   
+}
---
+++ Assets/Scripts/Portas/Porta_Default.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Porta_Default : MonoBehaviour
+{
+    BoxCollider triggerPlayers;
+    public bool StartingWave;
+
+    void Start()
+    {
+
+        triggerPlayers = FindObjectOfType<BoxCollider>();
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            StartingWave = true;
+        }
+    }
+
+
+}
