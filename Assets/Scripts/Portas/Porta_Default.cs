e7a200c020e5ef8f4eba0b9e054405e98a2a3c8d
Jordan Savio <jordan.savio.95@gmail.com>
2020-03-16 21:46:25 -0300

    1.1 - Porta Default - Versao 1
    Mecanismo da porta padrao do jogo.

--- Assets/Scripts/Portas/Porta_Default.cs
+++ Assets/Scripts/Portas/Porta_Default.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class Porta_Default : MonoBehaviour
 {
-    BoxCollider triggerPlayers;
-    public bool StartingWave;
+    BoxCollider triggerPlayers; //BoxCollider do Puzzle
+    bool StartingWave; //Colidir pra iniciar a wave
+
+    public float TimerToSpawn; //Tempo para iniciar a wave
+    public int WaveNumbers; //Quantidade de Waves
+    public int AtualWave; //WaveAtual do jogador
+    public int MonstersNumbers; //Quantidade de Monstros para Spawnar
+    public GameObject[] MonstersPrefab; //Prefab de cada Monstro
+    public Transform[] LocalSpawn; //Locais que eles vao spawnar
 
     void Start()
     {
+        triggerPlayers = FindObjectOfType<BoxCollider>(); //Pegar o Box desse Portao
+    }
 
-        triggerPlayers = FindObjectOfType<BoxCollider>();
+    void GoToSpawn()
+    {
+        Debug.Log("Iniciando Wave");
+
+        for (int i = 0; i < MonstersNumbers; i++)
+        {
+            int randomLocal = Random.Range(0, 9);
+            int randomMonster = Random.Range(0, 9);
+
+            Instantiate(MonstersPrefab[randomMonster], LocalSpawn[randomLocal].transform.position, LocalSpawn[randomLocal].transform.rotation);
+        }
+
+        AtualWave++; //Adiciona um contador ao final de dropar os monstros
+
 
     }
 
@@ -19,6 +41,12 @@ public class Porta_Default : MonoBehaviour
         if(other.gameObject.tag == "Player")
         {
             StartingWave = true;
+            if (StartingWave)
+            {
+                StartingWave = false;
+                Invoke("GoToSpawn", TimerToSpawn);              
+            }
+
         }
     }
 
