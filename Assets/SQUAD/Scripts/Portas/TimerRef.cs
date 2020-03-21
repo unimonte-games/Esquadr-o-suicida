7eacec34f7bdd7eaf85a481dfad534a69f2c1c6e
Jordan Savio <jordan.savio.95@gmail.com>
2020-03-21 01:27:28 -0300

    1.8 - Portao Timer

---
+++ Assets/SQUAD/Scripts/Portas/TimerRef.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerRef : MonoBehaviour
+{
+
+    public Porta_Timer P;
+    public RoomController R;
+    public int PlayerInt;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player1" && P.Player == false && R.TimerComplete == false)
+        {
+            P.Player = true;
+            PlayerInt = 1;
+        }
+
+        if (other.gameObject.name == "Player2" && P.Player == false && R.TimerComplete == false)
+        {
+            P.Player = true;
+            PlayerInt = 2;
+        }
+
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Player1" && P.Player && PlayerInt != 2)
+        {
+            P.Player = false;
+            PlayerInt = 0;
+            P.LastChance = true;
+           
+        }
+
+        if (other.gameObject.name == "Player2" && P.Player && PlayerInt != 1)
+        {
+            P.Player = false;
+            PlayerInt = 0;
+            P.LastChance = true;
+            
+        }
+    }
+
+}
