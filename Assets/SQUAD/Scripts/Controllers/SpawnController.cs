7504ae20e699350c982010be916a2071b6fc4063
Jordan Savio <jordan.savio.95@gmail.com>
2020-03-29 20:08:11 -0300

    2.26.1 - Spawn Dinamic - Controller

---
+++ Assets/SQUAD/Scripts/Controllers/SpawnController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public GameObject SpawnPrefab;
+    public Transform RoomSize;
+    Vector3 center;
+    Vector3 size;
+
+    public int Qtd;
+
+    void Start()
+    {
+        center.x = RoomSize.transform.position.x;
+        center.y = RoomSize.transform.position.y;
+        center.z = RoomSize.transform.position.z;
+
+        size.x = RoomSize.transform.localScale.x;
+        size.z = RoomSize.transform.localScale.z;
+
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        for (int i = 0; i <= Qtd; i++)
+        {
+            Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), 0, Random.Range(-size.z / 2, size.z / 2));
+
+            Instantiate(SpawnPrefab, pos, Quaternion.identity);
+        }
+        
+    }
+
+   
+}
