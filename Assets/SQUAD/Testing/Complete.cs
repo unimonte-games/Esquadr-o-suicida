7c1d5676a6b7ee622cfb8784c8f19160941ccc30
Jordan Savio <jordan.savio.95@gmail.com>
2020-03-30 00:43:07 -0300

    2.27.1 - Testing - Wave Default

---
+++ Assets/SQUAD/Testing/Complete.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Complete : MonoBehaviour
+{
+    public GameObject completeUI;
+    public RoomController RC;
+    public Porta_Default PD;
+
+    public Text Count;
+    public Text Defeat;
+
+   
+    void Update()
+    { 
+        Count.text = "" + PD.AtualMonsters;
+        Defeat.text = "" + PD.MonstersDestroy;
+    }
+}
