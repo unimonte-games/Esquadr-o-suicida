a4c963794ecafbb283c5eb8e608220021110dbe9
Jordan Savio <jordan.savio.95@gmail.com>
2020-03-29 22:50:54 -0300

    2.26.2 Spawn Dinamic - Area Check

---
+++ Assets/SQUAD/Scripts/Controllers/SpawnCheck.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCheck : MonoBehaviour
+{
+    public SpawnController SC;
+    public Transform SpawnObj;
+
+    private void Start()
+    {
+        Invoke("Atived", 1);
+    }
+
+    void Atived()
+    {
+        SC.List(SpawnObj);
+        Debug.Log("Spawn Acionado!");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == 8)
+        {
+            CancelInvoke("Atived"); 
+            Destroy(gameObject);
+            return;
+
+        }         
+    }
+
+}
--- Assets/SQUAD/Scripts/Controllers/SpawnController.cs
+++ Assets/SQUAD/Scripts/Controllers/SpawnController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class SpawnController : MonoBehaviour
 {
+
+    SpawnController SC_;
     public GameObject SpawnPrefab;
     public Transform RoomSize;
+    public Transform[] ListSpawn;
+    public Transform SceneTransformList;
     Vector3 center;
     Vector3 size;
 
-    public int Qtd;
+    public int Qtd = 100;
+    public int Acionados;
 
     void Start()
     {
+        SC_ = GetComponent<SpawnController>();
+
         center.x = RoomSize.transform.position.x;
         center.y = RoomSize.transform.position.y;
         center.z = RoomSize.transform.position.z;
@@ -29,9 +36,27 @@ public class SpawnController : MonoBehaviour
         {
             Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), 0, Random.Range(-size.z / 2, size.z / 2));
 
-            Instantiate(SpawnPrefab, pos, Quaternion.identity);
+            GameObject SpawnP = Instantiate(SpawnPrefab, pos, Quaternion.identity);
+            SpawnP.transform.parent = SceneTransformList;
+            SpawnP.GetComponent<SpawnCheck>().SC = SC_;
         }
         
+    }
+
+
+    public void List(Transform spw)
+    {
+        for (int i = 0; i <= Qtd; i++)
+        {
+            if(ListSpawn[i] == null)
+            {
+                ListSpawn[i] = spw;
+                Debug.Log("Add na List: " + i);
+                Acionados++;
+                i = Qtd+1; //encerrar
+            } 
+           
+        }
     }
 
    
