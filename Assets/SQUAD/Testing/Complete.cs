94c63d7ee28535b5cccaa634d9ce6bd2b518d86c
Jordan Savio <jordan.savio.95@gmail.com>
2020-03-30 01:00:12 -0300

    2.27.2 - Testing - Wave Multiple

--- Assets/SQUAD/Testing/Complete.cs
+++ Assets/SQUAD/Testing/Complete.cs
@@ -5,17 +5,22 @@ using UnityEngine.UI;
 
 public class Complete : MonoBehaviour
 {
-    public GameObject completeUI;
-    public RoomController RC;
+    
+   
     public Porta_Default PD;
+    
 
     public Text Count;
     public Text Defeat;
+    public Text AtualWave;
+    public Text WaveNumbers;
 
-   
+ 
     void Update()
     { 
         Count.text = "" + PD.AtualMonsters;
         Defeat.text = "" + PD.MonstersDestroy;
+        AtualWave.text = "" + PD.AtualWave;
+        WaveNumbers.text = "" + PD.WaveNumbers;
     }
 }
