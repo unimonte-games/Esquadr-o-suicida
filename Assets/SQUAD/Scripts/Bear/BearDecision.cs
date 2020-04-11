e63953c49c82c163920af858b632ddf63f1fae68
Jordan Savio <jordan.savio.95@gmail.com>
2020-04-10 21:38:59 -0300

    2.48 - Bear - Trap or bonus?

---
+++ Assets/SQUAD/Scripts/Bear/BearDecision.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearDecision : MonoBehaviour
+{
+
+    public bool Trap;
+    public bool Bonus;
+    public bool Gatilho;
+
+    public bool P1;
+    public bool P2;
+    public Player P;
+
+    private void FixedUpdate()
+    {
+        if (Gatilho)
+        {
+            Debug.Log("BOOM!");
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player1" && P1 && !Gatilho)
+        {
+            if (Trap)
+            {
+                Gatilho = true;
+                Debug.Log("Armadilha");
+            }
+
+            if (Bonus)
+            {
+                Debug.Log("Bonus");
+                this.gameObject.SetActive(false);
+            }
+        }
+
+        if (other.gameObject.name == "Player1" && P2 && !Gatilho)
+        {
+
+            if (Trap)
+            {
+                Gatilho = true;
+                Debug.Log("Armadilha");
+            }
+
+            if (Bonus)
+            {
+                Debug.Log("Bonus");
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+    
+}
---
+++ Assets/SQUAD/Scripts/Bear/BearTrapOrBonus.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearTrapOrBonus : MonoBehaviour
+{
+    
+
+    public BearDecision BD;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Player1")
+        {
+            if(!BD.P1 && !BD.P2)//ngm ter aproximado
+            {
+                BD.P1 = true;
+                BD.P = other.GetComponent<Player>();
+
+                int LocalSorte = Random.Range(0, 100);
+
+                if (LocalSorte <= 50)
+                {
+                    BD.Trap = true;
+                }
+
+                if (LocalSorte >= 51)
+                {
+                    BD.Bonus = true;
+                }
+
+            }
+
+            if(!BD.P1 && BD.P2)//O Player 2 já se aproximou
+            {
+                BD.Gatilho = true;
+            }
+            
+        }
+
+        if (other.gameObject.name == "Player2")
+        {
+            if (!BD.P1 && !BD.P2)//ngm ter aproximado
+            {
+                BD.P2 = true;
+                BD.P = other.GetComponent<Player>();
+
+                int LocalSorte = Random.Range(0, 100);
+
+                if (LocalSorte <= 70)
+                {
+                    BD.Trap = true;
+                }
+
+                if (LocalSorte >= 71)
+                {
+                    BD.Bonus = true;
+                }
+
+            }
+
+            if (BD.P1 && !BD.P2)//O Player 1 já se aproximou
+            {
+                BD.Gatilho = true;
+            }
+
+        }
+
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject.name == "Player1")
+        {
+            BD.Bonus = false;
+            BD.Trap = false;
+            BD.P1 = false;
+            BD.P2 = false;
+            BD.P = null;
+
+        }
+
+        if (other.gameObject.name == "Player2")
+        {
+            BD.Bonus = false;
+            BD.Trap = false;
+            BD.P1 = false;
+            BD.P2 = false;
+            BD.P = null;
+        }
+
+    }
+}
