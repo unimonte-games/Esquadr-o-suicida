eaae63ba62cf3179c0c94d6cfde8f9d93b8def77
Jordan Savio <jordan.savio.95@gmail.com>
2020-04-16 22:16:39 -0300

    2.56 - Dinamic Item - Baloon

---
+++ Assets/SQUAD/Scripts/Dinamic_Itens/Balao/BaloonMove.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaloonMove : MonoBehaviour
+{
+    public SpawnController SC;
+    public Transform[] ListToMove;
+
+    bool P1_inArea;
+    bool P2_inArea;
+
+    bool P1_ready;
+    bool P2_ready;
+
+    bool Go;
+
+    KeyCode P1;
+    KeyCode P2;
+
+    private void FixedUpdate()
+    {
+        if (P1_inArea && Input.GetKeyDown(P1) && !P1_ready && !Go)
+        {
+            P1_ready = true;
+
+            Debug.Log("Baloon Ativado no Player 1");
+        }
+
+        if (P2_inArea && Input.GetKeyDown(P2) && !P2_ready !Go)
+        {
+            P2_ready = true;
+
+            Debug.Log("Baloon Ativado no Player 2");
+        }
+
+        if (P1_ready && P2_ready && !Go)
+        {
+            Go = true;
+            ReOrganizeList();
+            Debug.Log("Balon Iniciado!");
+        }
+    }
+
+    void ReOrganizeList()
+    {
+        for (int i = 0; i <= SC.Acionados; i++)
+        {
+            ListToMove[i] = SC.ListSpawn[i];
+        }
+
+        Debug.Log("Lista Organizada");
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player1" && !P1_inArea)
+        {
+            if (!other.GetComponent<Player>().UsingItenDinamic)
+            {
+                P1_inArea = true;
+                P1 = other.GetComponent<Player>().Accept;
+            }
+        }
+
+        if (other.gameObject.name == "Player2" && !P2_inArea)
+        {
+            if (!other.GetComponent<Player>().UsingItenDinamic)
+            {
+                P2_inArea = true;
+                P2 = other.GetComponent<Player>().Accept;
+            }
+        }
+
+    }
+
+}
