9375b1fedbe147dd7adffe79965924aa3f0b26d1
Jordan Savio <jordan.savio.95@gmail.com>
2020-04-19 16:58:52 -0300

    2.63 - Fixed On! | Deleted Archives Old

--- Assets/SQUAD/Scripts/Controllers/LevelController.cs
+++ Assets/SQUAD/Scripts/Controllers/LevelController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class LevelController : MonoBehaviour
 {
     public int Level;
-    public Porta_Fixed PF;
     public int FixedOpen;
 
     public GameObject[] AllRoomList;
