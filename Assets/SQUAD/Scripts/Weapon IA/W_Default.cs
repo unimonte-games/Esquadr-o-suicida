31779315a317f97516d648c0b30fd9d2fc9a3f08
Jordan Savio <jordan.savio.95@gmail.com>
2020-04-30 17:49:53 -0300

    3.19 - Weapon Default

---
+++ Assets/SQUAD/Scripts/Weapon IA/W_Default.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class W_Default : MonoBehaviour
+{
+    public int weaponID;
+
+    public GameObject Shot;
+    public int Fire_Plant;
+    public int Fire_Tech;
+    public float FrameRate;
+    public int Mana;
+    public float Range;
+    public int Nivel;
+    public string Name;
+
+    public float Force;
+
+    public KeyCode Gatilho;
+    float countToShoting;
+
+    public Transform spawnShot;
+    public Player P;
+
+    void Start()
+    {
+        Gatilho = P.Gatilho;
+    }
+    private void FixedUpdate()
+    {
+        countToShoting += 0.1f;
+        if (Input.GetKeyDown(Gatilho) && countToShoting >= FrameRate)
+        {
+            countToShoting = 0f;
+            GameObject bullet = Instantiate(Shot, spawnShot.transform.position, Quaternion.identity) as GameObject;
+            Hit HitEnemy = bullet.GetComponent<Hit>();
+
+            HitEnemy.PlayerDestroy = P.PlayerType;
+            HitEnemy.time = Range;
+            HitEnemy.Hit_Plant = Fire_Plant;
+            HitEnemy.Hit_Tech = Fire_Tech;
+            P.ManaBar -= Mana;
+
+            bullet.GetComponent<Rigidbody>().AddForce(transform.forward * Force);
+        }
+
+    }
+}
