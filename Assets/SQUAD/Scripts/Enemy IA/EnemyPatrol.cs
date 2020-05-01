1e547aa3d1aa98031e22615193cb02be8209ac1c
Jordan Savio <jordan.savio.95@gmail.com>
2020-05-01 01:53:00 -0300

    3.22 - Materials Fix 3

---
+++ Assets/SQUAD/Scripts/Enemy IA/EnemyPatrol.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public float speed;
+    public float waitTime;
+    public float startWaitTime;
+
+    public Transform moveLocal;
+    public float minX;
+    public float maxX;
+    public float minZ;
+    public float maxZ;
+
+    void Start()
+    {
+        waitTime = startWaitTime;
+
+        moveLocal.position = new Vector3(Random.Range(minX, maxX), 0, Random.Range(minZ, maxZ));
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveLocal.position, speed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, moveLocal.position) < 0.2f)
+        {
+            if(waitTime <= 0)
+            {
+               
+                moveLocal.position = new Vector3(Random.Range(minX, maxX), 0, Random.Range(minZ, maxZ));
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
