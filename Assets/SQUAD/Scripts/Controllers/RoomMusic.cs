6c3807fcfe5c304eeed50cf044235819cb006df6
Jordan Savio <jordan.savio.95@gmail.com>
2020-05-11 19:17:09 -0300

    3.71.1 - Sound System - Controller

---
+++ Assets/SQUAD/Scripts/Controllers/RoomMusic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomMusic : MonoBehaviour
+{
+    SoundController SC;
+    public AudioSource[] RoomList;
+    int randomNumber;
+    public int MusicStep_min;
+    public int MusicStep_max;
+
+    void Start()
+    {
+        SC = FindObjectOfType<SoundController>();
+
+        for (int i = 0; i < 15; i++)
+        {
+            randomNumber = Random.Range(MusicStep_min, MusicStep_max);
+            RoomList[i] = SC.AllSounds[randomNumber];
+
+        }
+
+    }
+
+}
--- Assets/SQUAD/Scripts/Controllers/SoundController.cs
+++ Assets/SQUAD/Scripts/Controllers/SoundController.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class SoundController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioSource[] AllSounds;
+    
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
