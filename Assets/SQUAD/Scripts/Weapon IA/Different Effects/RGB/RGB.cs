21070d998a3867cda786dab6754fac13ac2af150
Jordan Savio <jordan.savio.95@gmail.com>
2020-05-11 00:46:55 -0300

    3.67 - Item List 58-60

---
+++ Assets/SQUAD/Scripts/Weapon IA/Different Effects/RGB/RGB.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RGB : MonoBehaviour
+{
+    public MeshRenderer MR;
+    public Material[] Color;
+    
+    int Number;
+
+    private void OnEnable()
+    {
+        Number = Random.Range(0, 14);
+        MR.material = Color[Number];
+    }
+}
