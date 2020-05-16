94d86a63a97fcda3fa2bdb6903811f34cb54fc46
Jordan Savio <jordan.savio.95@gmail.com>
2020-05-16 01:42:01 -0300

    3.78.4 - Enemy Anin - Robo

--- Assets/SQUAD/Scripts/Enemy IA/Enemy 7 - Elite/Enemy7_Attack.cs
+++ Assets/SQUAD/Scripts/Enemy IA/Enemy 7 - Elite/Enemy7_Attack.cs
@@ -26,12 +26,26 @@ public class Enemy7_Attack : MonoBehaviour
     private void OnDisable()
     {
         CancelInvoke("CombatCountDown");
+        CancelInvoke("Effect");
     }
-
 
     void CombatCountDown()
     {
         {
+            
+
+            TimeToAttack = Random.Range(3, 10);
+
+            Invoke("Effect", 0.5f);
+
+        }
+    }
+
+
+    void Effect()
+    {
+        {
+            ES.A_Attack();
             TimeToAttack = Random.Range(3, 5);
 
             GameObject bullet1 = Instantiate(Shot, Spawn.position, Quaternion.identity) as GameObject;
